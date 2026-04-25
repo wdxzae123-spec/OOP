e0396315fcc534b4c643ced79ea2a2d0664b2c5d
Xorset <wdxzae@mail.ru>
2026-04-25 10:56:55 +0500

    ready

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -2,7 +2,13 @@ package org.skypro.skyshop.product;
 
 public class Product {
     private final String name;
-    private final int price; // стоимость в целых единицах (например, рубли)
+    private final int price;
+
+    /** стоимость в целых единицах (например, рубли)
+     *
+     * @param name
+     * @param price
+     */
 
     public Product(String name, int price) {
         this.name = name;
